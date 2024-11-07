21b9d153893ecebf945f7fc353dd3405153b668d
mattinpgh <matthew.a.w.smith@gmail.com>
2024-11-06 21:30:52 -0500

    Add player movement: l+r rotations

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ def main():
     
     
     while True:
-        screen.fill((0,0,0))
-        player_model.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill((0,0,0))
+        player_model.update(dt)
+        player_model.draw(screen)
         
         pygame.display.flip()
         dt = clock.tick(60)/1000
